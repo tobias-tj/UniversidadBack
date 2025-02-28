6165e8bb77c21e958e81a4cc718cbfa95d941c9d
tobias-tj <https.tobias.jara.404@gmail.com>
2025-02-27 21:54:55 -0300

    Se ajusta la ip para el cors

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 3000;
 // Configuración de CORS
 const corsOptions = {
   origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
+    'http://161.35.53.140:5173',
+    'http://161.35.53.140:5174',
     'http://localhost:3010',
   ],
 };
