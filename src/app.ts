bc374b737a9cd9a547f8adc34dd8d4446a4df83b
tobias-tj <https.tobias.jara.404@gmail.com>
2025-03-26 15:02:59 -0300

    Limpieza para test

--- src/app.ts
+++ src/app.ts
@@ -7,9 +7,6 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-console.log('📌 DATABASE_URL:', process.env.DATABASE_URL);
-logger.info('📌 DATABASE_URL:', process.env.DATABASE_URL);
-
 // Configuración de CORS
 const corsOptions = {
   origin: [
