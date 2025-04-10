ac050163e2d5d2b207334992987fcce31902db6d
Sam281055 <127691992+Sam281055@users.noreply.github.com>
2025-04-10 23:22:53 +0000

    first commit

--- src/interface/controllers/anuncios.controller.ts
+++ src/interface/controllers/anuncios.controller.ts
@@ -16,29 +16,44 @@ export class AnunciosController {
   async getAllAnuncios(req: Request, res: Response, next: NextFunction) {
     try {
       const token = req.headers.authorization?.split(' ')[1];
+      const onlyUnread = req.query.onlyUnread === 'true'; // Extraer la bandera como booleano
       if (!token) {
         return res.status(401).json({ error: 'Token no proporcionado' });
       }
-
+  
       // Decodificar token
       const decoded = decodeToken(token);
       if (!decoded?.connectionDb) {
         return res.status(401).json({ error: 'Token inválido' });
       }
-
+  
+      // Obtener anuncios según la bandera
       const anunciosList = await this.getTotalAnuncios.execute(
         decoded.connectionDb,
+        onlyUnread // Pasar la bandera al servicio
       );
+  
       console.log(anunciosList);
       logger.info('Termina proceso para obtener anuncios');
-
+  
+      // Conteo de anuncios
+      const totalNotifications = anunciosList.length;
+      const unreadNotifications = anunciosList.filter((anuncio: any) => !anuncio.read).length;
+  
       if (!anunciosList.length) {
-        logger.info('No se encontraron anuncios sin ver');
+        logger.info('No se encontraron anuncios');
         res.status(204).send(); // No Content
         return;
       }
-
-      res.status(200).json({ data: anunciosList });
+  
+      // Respuesta con datos y conteo
+      res.status(200).json({
+        data: anunciosList,
+        count: {
+          totalNotifications,
+          unreadNotifications,
+        },
+      });
     } catch (error) {
       logger.error('Error en el controlador de anuncios', { error });
       next(error);
