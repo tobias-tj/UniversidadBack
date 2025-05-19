1361088a3620c2153b534f64e99b0f1a6ecde955
tobias-tj <https.tobias.jara.404@gmail.com>
2025-05-19 16:10:23 -0300

    Se saca links innecesarios

--- src/interface/routes/anunciosRoutes.ts
+++ src/interface/routes/anunciosRoutes.ts
@@ -2,42 +2,18 @@ import { Router, Request, Response, NextFunction } from 'express';
 import { AnuncioRepository } from '../../infrastructure/repositories/anuncios/anuncioRepository';
 import { AnunciosController } from '../controllers/anuncios.controller';
 import { GetAnuncios } from '../../usecases/anuncios/GetAnuncios';
-import { createAnuncioValidation } from '../../domain/interfaces/middleware/createAnuncioValidation';
-import { CreateAnuncio } from '../../usecases/anuncios/CreateAnuncio';
-import { updateAnunciosByIdValidation } from '../../domain/interfaces/middleware/updateAnuncioByIdValidation';
-import { UpdateAnuncioById } from '../../usecases/anuncios/UpdateAnuncioById';
 
 const router = Router();
 
 const anuncioRepository = new AnuncioRepository();
 const getAnuncio = new GetAnuncios(anuncioRepository);
-const createAnuncio = new CreateAnuncio(anuncioRepository);
-const updateAnuncio = new UpdateAnuncioById(anuncioRepository);
 
-const anunciosController = new AnunciosController(
-  getAnuncio,
-  createAnuncio,
-  updateAnuncio,
-);
+const anunciosController = new AnunciosController(getAnuncio);
 
 router.get(
   '/getAllAnuncios',
   (req: Request, res: Response, next: NextFunction) =>
     anunciosController.getAllAnuncios(req, res, next),
-);
-
-router.post(
-  '/createAnuncio',
-  [...createAnuncioValidation],
-  (req: Request, res: Response, next: NextFunction) =>
-    anunciosController.createAnuncios(req, res, next),
-);
-
-router.patch(
-  '/updateStatusAnuncio',
-  [...updateAnunciosByIdValidation],
-  (req: Request, res: Response, next: NextFunction) =>
-    anunciosController.updateAnunciosById(req, res, next),
 );
 
 export { router as anunciosRoutes };
